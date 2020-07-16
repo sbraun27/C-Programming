baff13b02884812bf5323881fbebbbf814811a2a
Coursera Learner <nobody@nowhere.nul>
2020-07-16 20:52:45 +0000

    Finished assignment

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -45,13 +45,9 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (y_offset + size2).  Call this h
   int h = max(size1, (y_offset + size2));
   //count from 0 to h. Call the number you count with y
-  for (int y = 0; y <= h; y++) {
+  for (int y = 0; y < h; y++) {
     //count from 0 to w. Call the number you count with x
-    for (int x = 0; x <= w; x++) {
-      int xinRange = isInRange(x, x_offset, size2);
-      int xatBorder = isAtBorder(x, x_offset, size2 - 1);
-      int yinRange = isInRange(y, y_offset, size2);
-      int yatBorder = isAtBorder(y, y_offset, size2-1);
+    for (int x = 0; x < w; x++) {
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
@@ -59,11 +55,11 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-      if (((xinRange == 1) && (yatBorder == 1)) || ((yinRange == 1) && (xatBorder == 1))) {
+      if (((x >= x_offset && x < x_offset + size2) && (y == y_offset || y == y_offset + size2 - 1)) || ((y >= y_offset && y < y_offset + size2) && (x == x_offset || x == x_offset + size2 -1))) {
 	printf("*");
       }
 
-      else if (((x < size1) && ((y == 0) || (y == (size1 - 1)))) || ((y < size1) && ((x == 0) || (x == (size1 - 1))))) {
+      else if (((x < size1) && (y == 0 || y == size1 -1 )) || ((y < size1) && (x == 0 || x == size1 - 1))) {
 	printf("#");
       }
 	
@@ -84,3 +80,4 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
     }
   }
 }
+
