68bee0ba4e020cd1efea175af98479a96f8b558f
Coursera Learner <nobody@nowhere.nul>
2020-07-23 12:25:49 +0000

    Submitting assignment

---
+++ 07_retirement/retirement.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _retire_info {
+  int months;
+  double contribution;
+  double rate_of_return;
+} retire_info;
+
+void retirement (int startAge, double initial, retire_info working, retire_info retired) {
+  double accountBalance = initial;
+  int currentAge = startAge;
+  
+  for (int i=0; i <working.months; i++) {
+    int years = currentAge / 12;
+    int months = currentAge % 12;
+    printf("Age %3d month %2d you have $%.21f\n", years, months, accountBalance);
+    accountBalance = accountBalance * (1 + working.rate_of_return) + working.contribution;
+  }
+
+  for (int i = 0; i < retired.months; i++) {
+    int years = currentAge / 12;
+    int months = currentAge % 12;
+    printf("Age %3d month %2d you have$%.21f\n", years, months, accountBalance);
+    accountBalance = accountBalance * (1 + retired.rate_of_return) + retired.contribution;
+  }
+  return;
+}
+
+int main (void) {
+  retire_info Working;
+  Working.months = 489;
+  Working.contribution = 1000;
+  Working.rate_of_return = (.045 / 12);
+
+  retire_info Retired;
+  Retired.months = 384;
+  Retired.contribution = -4000;
+  Retired.rate_of_return = (.01 / 12);
+
+  retirement(327, 21435, Working, Retired);
+
+  return 0;
+  
+
+}
