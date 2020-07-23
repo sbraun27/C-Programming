927e0761cc1bc96af4aff3526b332471c52364a0
Coursera Learner <nobody@nowhere.nul>
2020-07-23 12:28:02 +0000

    Submitting assignment

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _retire_info {
+struct _retire_info {
   int months;
   double contribution;
   double rate_of_return;
-} retire_info;
+};
+
+typedef struct _retire_info retire_info;
 
 void retirement (int startAge, double initial, retire_info working, retire_info retired) {
   double accountBalance = initial;
