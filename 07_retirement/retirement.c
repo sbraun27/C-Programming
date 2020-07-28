4dd96002977b91f9702a0921767289a38ad93e58
Coursera Learner <nobody@nowhere.nul>
2020-07-28 00:13:42 +0000

    Submit

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -11,11 +11,12 @@ typedef struct _retire_info retire_info;
 
 void retirement (int startAge, double initial, retire_info working, retire_info retired) {
   double accountBalance = initial;
-  int currentAge = startAge;
+  int currentAge = startAge-1;
   
   for (int i=0; i <working.months; i++) {
     int years = currentAge / 12;
     int months = currentAge % 12;
+    currentAge += 1;
     printf("Age %3d month %2d you have $%.21f\n", years, months, accountBalance);
     accountBalance = accountBalance * (1 + working.rate_of_return) + working.contribution;
   }
@@ -23,6 +24,7 @@ void retirement (int startAge, double initial, retire_info working, retire_info 
   for (int i = 0; i < retired.months; i++) {
     int years = currentAge / 12;
     int months = currentAge % 12;
+    currentAge += 1;
     printf("Age %3d month %2d you have$%.21f\n", years, months, accountBalance);
     accountBalance = accountBalance * (1 + retired.rate_of_return) + retired.contribution;
   }
@@ -33,12 +35,12 @@ int main (void) {
   retire_info Working;
   Working.months = 489;
   Working.contribution = 1000;
-  Working.rate_of_return = (.045 / 12);
+  Working.rate_of_return = (.045 / 12.0);
 
   retire_info Retired;
   Retired.months = 384;
   Retired.contribution = -4000;
-  Retired.rate_of_return = (.01 / 12);
+  Retired.rate_of_return = (.01 / 12.0);
 
   retirement(327, 21435, Working, Retired);
 
